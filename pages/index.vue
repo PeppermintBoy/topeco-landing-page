f2b944057039350c3af9be73d9194e3afd8a5cf7
Nathan Shosuke Muto <yoyointheho@gmail.com>
2023-03-21 22:59:09 +0900

    Update landing page with dummy info

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,13 @@ import { Landing } from '~~/.nuxt/components';
 
 <template>
 	<div>
-		<h1>Home test</h1>
+		<!-- <h1>Home test</h1>
 		<p>This is Home page</p>
-		<nuxt-link to="/about">Go to About</nuxt-link>
+		<nuxt-link to="/about">Go to About</nuxt-link> -->
 		<Landing />
 	</div>
 </template>
+
+<style>
+@import '~/assets/css/global.css';
+</style>
