f84f58b714fd2944398b563c228d1cb8e5779bf7
Nathan Shosuke Muto <yoyointheho@gmail.com>
2023-03-21 21:02:35 +0900

    Landing page base created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	target: 'static', // default is 'server'
+	css: ['~/node_modules/bootstrap/dist/css/bootstrap.min.css'],
+	plugins: [
+		{
+			src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+			mode: 'client',
+		},
+	],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
+import { Landing } from '~~/.nuxt/components';
+
 <template>
 	<div>
 		<h1>Home test</h1>
 		<p>This is Home page</p>
 		<nuxt-link to="/about">Go to About</nuxt-link>
+		<Landing />
 	</div>
 </template>
